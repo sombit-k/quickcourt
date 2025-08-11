2e772803d455d573adf24826a50946079b78eae7
sombit-k <sombitkarmakar018@gmail.com>
2025-08-11 10:03:36 +0530

    Add AuthLayout component and initial page structure for sign-in, sign-up, and profile

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -0,0 +1,11 @@
+// import { SignIn } from '@clerk/nextjs'
+import React from 'react'
+
+const page = () => {
+  return (
+    // <SignIn/>
+    <div>Hello</div>
+  )
+}
+
+export default page
---
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,11 @@
+// import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+const page = () => {
+  return (
+    // <SignUp/>
+    <div>Sign Up Page</div>
+  )
+}
+
+export default page
